03fb0e873d2160a5b820ba64073cbc46038a1baa
MECIOSLAPTOP\oceres <oceres@hotmail.com>
2024-08-14 21:02:06 -0400

    REACTJS-01: Componentes

---
+++ src/components/Github.jsx
@@ -0,0 +1,16 @@
+const Github = () => { 
+    return (
+        <>
+            <a
+                className="App-link"
+                href="https://github.com/0ceres/desarrollo-frontend-react"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Mi proyecto React (GitHub)
+            </a>
+        </>
+    );
+};
+
+export default Github;
