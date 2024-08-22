b73761ab2f5bb0981ea334a728c18b40a4fdba3b
MECIOSLAPTOP\oceres <oceres@hotmail.com>
2024-08-21 22:08:07 -0400

    REACTJS-01: Trying to solve issue

--- src/router/router.js
+++ src/router/router.js
@@ -5,7 +5,7 @@ import Product from "../screens/Product";
 import App from "../App";
 import LoginForm from "../screens/Forms/LoginForm";
 
-const basename = process.env.NODE_ENV === 'production' ? '/clase1-introduccion' : '/';
+const basename = process.env.NODE_ENV === 'production' ? '/desarrollo-frontend-react' : '/';
 
 const routes = createBrowserRouter([
     { 
